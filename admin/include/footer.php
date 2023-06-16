7d5ed18c6f12c3498a3b380bbd9ad807225604bb
cy20001yadav <cy20001yadav@gmail.com>
2023-06-16 10:21:57 +0530

    first commit

---
+++ admin/include/footer.php
@@ -0,0 +1,8 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; 2019 Complaint Management System </b> All rights reserved.
+		</div>
+	</div>
+	
---
+++ js/includes/footer.php
@@ -0,0 +1,12 @@
+ <footer class="site-footer">
+          <div class="text-center">
+             &copy; 2019 Complaint Management System </b> All rights reserved.
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+		         
+		 </div>
+		 <div style="margin-left:250px;">
+		 
+	</div>
+      </footer>
